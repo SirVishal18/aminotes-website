bc421d0162ba01142fd2a02b18d0c31fde12c71f
Vishal Shukla <78338976+SirVishal18@users.noreply.github.com>
2021-06-10 20:55:16 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<nav class="navbar sticky-top custom-nav">
+    <div class="container brand-tit">
+      <img src="http://aminotes.ezyro.com/ico/main-ico.png" class="brand-logo"><a class="navbar-tit" href="http://aminotes.ezyro.com/"> Ami Notes</a>
+    </div>
+    <div class="container nav-ob">
+      <a class="btn btn-primary" href="/notes/">Notes</a>
+      <a class="btn btn-primary" style="margin-left:10px" href="/blog/">Blogs</a>
+    </div>
+</nav>
